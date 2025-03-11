3b8dc62532f1902f77ac23ff1c82344f0ccea8bb
Sayeed Bin Noman <bluespace558@gmail.com>
2025-03-12 02:59:22 +0600

    rockpaper_project03.py

--- rockpaper_project03.py
+++ rockpaper_project03.py
@@ -4,37 +4,44 @@ import random
 element_list = ["ROCK", "PAPER" ,  "SCISSOR"]
 
 
-user_choice = input("\n\n\nEnter your choice (ROCK / PAPER / SCISSOR): ").upper()
-computer_choice = random.choice(element_list)
+def main():
+    while True:
+        
+        user_choice = input("Enter your choice (ROCK / PAPER / SCISSOR): ").upper()
+        computer_choice = random.choice(element_list)
 
-print(f"\n\nUser move: {user_choice} \nComputer move: {computer_choice}\n\n")
-
-
-
-if user_choice == computer_choice:
-    print("Both has same move! Match tie....")
-
-
-elif user_choice == "ROCK":
-    if computer_choice == "PAPER":
-        print("PAPER covered the ROCK : Computer WIN!!\U0001F4BB \U0001F389")
-    else:
-        print("ROCK smashed the SCISSOR : User WIN!!\U0001F9D4 \U0001F389 ")
-elif user_choice == "PAPER":
-    if computer_choice == "ROCK":
-        print("PAPER covered the ROCK : User WIN!!\U0001F9D4 \U0001F389")
-    else:
-        print("SCISSOR cut the PAPER : Computer WIN!!\U0001F4BB \U0001F389")
-
-elif user_choice == "SCISSOR":
-    if computer_choice == "PAPER":
-        print("SCISSOR cut the PAPER : User WIN!!\U0001F9D4 \U0001F389")
-    else:
-        print("ROCK smashed the SCISSOR : Computer WIN!!\U0001F4BB \U0001F389")
-else:
-    print("User move is not correct")
+        print(f"\nUser move: {user_choice} \nComputer move: {computer_choice}\n")
+        
+        if user_choice == computer_choice:
+            print("Both has same move! Match tie....")
 
 
-print("\n\n")
+        elif user_choice == "ROCK":
+            if computer_choice == "PAPER":
+                print("PAPER covered the ROCK : Computer WIN!!\U0001F4BB \U0001F389")
+            else:
+                print("ROCK smashed the SCISSOR : User WIN!!\U0001F9D4 \U0001F389 ")
+        elif user_choice == "PAPER":
+            if computer_choice == "ROCK":
+                print("PAPER covered the ROCK : User WIN!!\U0001F9D4 \U0001F389")
+            else:
+                print("SCISSOR cut the PAPER : Computer WIN!!\U0001F4BB \U0001F389")
+
+        elif user_choice == "SCISSOR":
+            if computer_choice == "PAPER":
+                print("SCISSOR cut the PAPER : User WIN!!\U0001F9D4 \U0001F389")
+            else:
+                print("ROCK smashed the SCISSOR : Computer WIN!!\U0001F4BB \U0001F389")
+        else:
+            print("User move is not correct")
+            break
 
 
+print("\n")
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
